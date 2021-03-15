aa7f22964c520ccbf2301ebbc3a06336e913c0ca
Tommaso Bianucci <bianucci@mpi-cbg.de>
2021-03-15 11:45:17 +0000

    Fix plots so that they use TrueType fonts instead of Type 3 fonts

--- SAIC.jl
+++ SAIC.jl
@@ -12,22 +12,27 @@ include("SAIC_structs.jl")
 include("SAIC_SSA.jl")
 
 module Models
-using ..SAIC	# Use exported function from the parent module
-using InlineExports			# Provides the @export macro
-using Random, Distributions
-include("SAIC_models.jl")
+	using ..SAIC	# Use exported function from the parent module
+	using InlineExports			# Provides the @export macro
+	using Random, Distributions
+	include("SAIC_models.jl")
 end # submodule
 
 module Figures
-using ..SAIC	# Use exported function from the parent module
-using InlineExports			# Provides the @export macro
-using LaTeXStrings			# For using LaTeX in plot labels (e.g. L"X^2")
-using Measures				# Required to use `mm` or `pt` as units for plot margins
-using Plots; pyplot()
-import KernelDensity 		# Required for KernelDensityEstimation
-using Serialization 		# Required for saving/loading simulation results
-using Parameters 			# Provides the @unpack macro
-include("SAIC_figures.jl")
+	using ..SAIC	# Use exported function from the parent module
+	using InlineExports			# Provides the @export macro
+	using LaTeXStrings			# For using LaTeX in plot labels (e.g. L"X^2")
+	using Measures				# Required to use `mm` or `pt` as units for plot margins
+	using Plots; pyplot()
+	# Force the use of TrueType fonts in plots (ref: http://phyletica.org/matplotlib-fonts/ )
+	# This is to avoid errors because of Type 3 fonts.
+	Plots.PyPlot.matplotlib.rc("pdf", fonttype=42)
+	Plots.PyPlot.matplotlib.rc("ps", fonttype=42)
+	#
+	import KernelDensity 		# Required for KernelDensityEstimation
+	using Serialization 		# Required for saving/loading simulation results
+	using Parameters 			# Provides the @unpack macro
+	include("SAIC_figures.jl")
 end # submodule
 
 end  # module
